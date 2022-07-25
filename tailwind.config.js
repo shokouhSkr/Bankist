c3e8263f74a87fdf652dc0f88fe5d06e2556eccf
shokouhSkr <shokouhskr@gmail.com>
2022-07-26 01:01:11 +0430

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}", "./src/js/*.{html,js}"],
+  darkMode: "class",
+  theme: {
+    container: {
+      center: true,
+      // padding: "1rem",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Vazir"],
+        // Nunito: ["Nunito", "'sans-serif'"],
+      },
+      screens: {
+        "2xs": "375px",
+        xs: "425px",
+        "3xl": "1440px",
+      },
+    },
+  },
+  plugins: [],
+};
