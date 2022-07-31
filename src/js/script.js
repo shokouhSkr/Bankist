35f8be5f3a13ed6409cf8e4cfc9683f7fe57e0bb
shokouhSkr <shokouhskr@gmail.com>
2022-07-31 16:59:07 +0430

    displayMovements function added

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,68 @@
 "use strict";
+
+// Data
+const account1 = {
+  owner: "Shokouh Askari",
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: "Jessica Davis",
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: "Steven Thomas Williams",
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: "Sarah Smith",
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+/////////////////////////////////////////////////
+// Elements
+const containerMovements = document.querySelector(".movements");
+
+/////////////////////////////////////////////////
+// Functions
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = "";
+
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? "deposit" : "withdrawal";
+    const colorType =
+      type === "deposit"
+        ? "bg-gradient-to-tl from-[#39b385] to-[#9be15d]"
+        : "bg-gradient-to-tl from-[#e52a5a] to-[#ff585f]";
+
+    const html = `
+        <div class="movement-row">
+          <div class="details">
+            <span class="type ${colorType}">${i + 1} ${type}</span>
+            <span class="text-[#666]">3 days ago</span>
+          </div>
+          <div>
+            <span>${mov} €</span>
+          </div>
+        </div>
+    `;
+
+    containerMovements.insertAdjacentHTML("afterbegin", html);
+  });
+};
+
+displayMovements(account1.movements);
+///////////////////////////////////////
+// Event handlers
