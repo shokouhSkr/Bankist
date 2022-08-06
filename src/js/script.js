23b6a49376a8ff99df8ee6f3b00768b272c43efe
shokouhSkr <shokouhskr@gmail.com>
2022-08-06 19:40:19 +0430

    Different data_Contains movement dates, currency and locale

--- src/js/script.js
+++ src/js/script.js
@@ -2,10 +2,23 @@
 
 // Data
 const account1 = {
-  owner: "Shokouh Askari",
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  owner: "Jonas Schmedtmann",
+  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+
+  movementsDates: [
+    "2019-11-18T21:31:17.178Z",
+    "2019-12-23T07:42:02.383Z",
+    "2020-01-28T09:15:04.904Z",
+    "2020-04-01T10:17:24.185Z",
+    "2020-05-08T14:11:59.604Z",
+    "2020-07-26T17:01:17.194Z",
+    "2020-07-28T23:36:17.929Z",
+    "2020-08-01T10:51:36.790Z",
+  ],
+  currency: "EUR",
+  locale: "pt-PT", // de-DE
 };
 
 const account2 = {
@@ -13,30 +26,29 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+
+  movementsDates: [
+    "2019-11-01T13:15:33.035Z",
+    "2019-11-30T09:48:16.867Z",
+    "2019-12-25T06:04:23.907Z",
+    "2020-01-25T14:18:46.235Z",
+    "2020-02-05T16:33:06.386Z",
+    "2020-04-10T14:43:26.374Z",
+    "2020-06-25T18:49:59.371Z",
+    "2020-07-26T12:01:20.894Z",
+  ],
+  currency: "USD",
+  locale: "en-US",
 };
 
-const account3 = {
-  owner: "Steven Thomas Williams",
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
-};
-
-const account4 = {
-  owner: "Sarah Smith",
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
-};
-
-const accounts = [account1, account2, account3, account4];
+const accounts = [account1, account2];
 
 /************************************************/
 // Elements
 const containerMovements = document.querySelector(".movements");
 const containerApp = document.querySelector(".app");
 
-const labelBalance = document.querySelector(".balance");
+const labelBalance = document.querySelector(".balance__value");
 const labelSumIn = document.querySelector(".income");
 const labelSumOut = document.querySelector(".outcome");
 const labelInterest = document.querySelector(".interest");
@@ -132,6 +144,8 @@ let currentAccount;
 
 btnLogin.addEventListener("click", function (e) {
   e.preventDefault();
+  e.stopPropagation();
+  e.stopImmediatePropagation();
 
   currentAccount = accounts.find((acc) => acc.username === inputLoginUsername.value);
   // console.log(currentAccount);
